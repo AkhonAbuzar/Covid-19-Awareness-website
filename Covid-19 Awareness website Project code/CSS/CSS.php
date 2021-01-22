27cc48ab08dab477c13f5a65576ae10142b1e6fd
AkhonAbuzar <77823970+AkhonAbuzar@users.noreply.github.com>
2021-01-22 11:14:10 +0530

    Add files via upload

---
+++ Covid-19 Awareness website Project code/CSS/CSS.php
@@ -0,0 +1,53 @@
+<style type="text/css">
+html{
+	scroll-behavior:smooth;
+}
+	
+*{margin: 0;padding: 0;box-sizing: border-box;font-family: 'Mulish', sans-serif;}
+
+.nav_style{
+	background-color: #b3ffb3!important;
+}
+.nav_style a{color:black;}
+/*///////////// main header\\\\\\\\\\\*/
+
+
+.main-header{
+	height:450px;
+	width:100%;
+}
+.rightside h1{
+	font-size: 3rem;
+}
+
+.corona_rot img{
+	animation:gocorona 3s linear infinite;
+}
+@keyframes gocorona{
+	0%{transform:rotate(0);}
+	100%{transform: rotate(360deg)}
+}
+.rightside h1{
+	font-size: 1rem;
+}
+.sub_section p{
+	font-size: 1.25rem;
+}
+
+.footer_style{
+	background-color:#b3ffb3!important;
+}
+.footer_style p{
+	margin-bottom: 0!important;
+
+}
+.corona_update h3{
+font-size: 2.5rem;
+color: #ff7675;
+
+}
+.corona_update{
+	margin:0 0 30px 0;
+
+}
+</style>
---
+++ Covid-19 Awareness website Project code/index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php include 'link/links.php'; ?>
+	<?php include 'CSS/CSS.php'; ?>
+</head>  
+<body>
+<nav class="navbar navbar-expand-lg nav_style  p-3">
+  <a class="navbar-brand pl-5" href="#">COVID-19</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto pr-5 text-capitalize ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      	<a class="nav-link" href="#aboutid">about</a>
+      </li>
+       <li class="nav-item">
+      	<a class="nav-link" href="#sympid">symptoms</a>
+      </li>
+       <li class="nav-item">
+      	<a class="nav-link" href="#preventid">prevention</a>
+      </li>
+      <li class="nav-item">
+      	<a class="nav-link" href="#contactid">contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="main_header">
+	<div class="row w-100 h-100 pt-5">
+		<div class="col-lg-5 col-md-5 col-12 order-lg-1 order-2">
+		    <div class="leftside w-100 h-100 d-flex justify-content-center align-items-center ml-5">
+		    	<img src="images/img 2.jpg" width="300" height="300">
+		    </div>
+		</div>
+		<div class="col-lg-7 col-md-7 col-12 order-lg-2 order-1">
+		<div class="rightsight w-100 h-100 d-flex justify-content-center align-items-center">
+			<h1>Let's Stay At Home For You,For Us And For Our Country To Fight Against Cor <span class="corona_rot"><img src="images/img 1.jpg" width="30" height="30">
+			</span>na Virus</h1>
+			
+		</div>		
+		</div>
+
+	</div>
+	
+</div>
+
+<!--*************Corona latest Update***************-->
+<section class="corona_update mt-5">
+	<div class="mb-3">
+		<h3 class="text-uppercase text-center">covid-19 updates</h3>
+	</div>
+
+	<div class="d-flex justify-content-around align-items-center">
+		<div>
+			<h1 class="count">1,524,266</h1>
+			<p>Passengers screened at airport</p>
+		</div>
+
+		<div>
+			<h1 class="count">512</h1>
+			<p>Active COVID-19 cases</p>
+		</div>
+
+		<div>
+			<h1 class="count">40</h1>
+			<p>Current discharged cases</p>
+		</div>
+
+		<div>
+			<h1 class="count">9</h1>
+			<p>Death cases</p>
+		</div>
+
+	</div>
+		
+	</div>
+	
+</section>
+
+
+
+
+</section>
+
+<!--*************about section***************-->
+<div class="container-fluid sub_section pt-5 pb-5 "id="aboutid">
+	<div class="section_header text-center mb-5 mt-4">
+		<h1> About COVID-19</h1>
+	</div>
+	<div class="row pt-5">
+		<div class="col-lg-5 col-md-6 col-12 ml-5 ">
+			<img src="images/corona image 1.jpg" class="img-fluid">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12 mt-2">
+		<h2>What is COVID-19</h2>
+        <p>Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.</p>
+        <p>Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV). A novel coronavirus (nCoV) is a new strain that has not been previously identified in humans.</p>
+    </div>
+</div>
+
+<!--*************Corona Symptoms***************-->
+<div class="container-fluid pt-5 pb-5 "id="sympid">
+	<div class="section_header text-center mb-5 mt-4">
+		<h1> Corona Symptoms</h1>
+	</div>
+
+
+    <div class="container">
+	    <div class="row">
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+			    <figure class="text-center">
+				<img src="images/corona 1.jpg" class="img-fluid" width="120" height="120">
+				<figcaption>cough</figcaption>
+			    </figure>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+			        <figure class="text-center">
+				    <img src="images/corona 2.jpg" class="img-fluid" width="120" height="120">
+				    <figcaption>Fever</figcaption>
+			        </figure>
+		        </div>
+
+
+		        <div class="col-lg-4 col-md-4 col-12 mt-5">
+			        <figure class="text-center">
+				    <img src="images/corona 4.jpg" class="img-fluid" width="120" height="120">
+				    <figcaption>Muscle pain</figcaption>
+			        </figure>
+		        </div>
+
+		        <div class="col-lg-4 col-md-4 col-12 mt-5">
+			        <figure class="text-center">
+				    <img src="images/corona 5.jpg" class="img-fluid" width="120" height="120">
+				    <figcaption>Short of breath or difficulty breathing</figcaption>
+			        </figure>
+		        </div>
+
+		        <div class="col-lg-4 col-md-4 col-12 mt-5">
+			        <figure class="text-center">
+				    <img src="images/corona 6.jpg" class="img-fluid" width="120" height="120">
+				    <figcaption>Sore throat</figcaption>
+			        </figure>
+		        </div>
+
+		        <div class="col-lg-4 col-md-4 col-12 mt-5">
+			        <figure class="text-center">
+				    <img src="images/corona 7.jpg" class="img-fluid" width="120" height="120">
+				    <figcaption>New loss of taste or smell</figcaption>
+			        </figure>
+		        </div>
+		</div>
+    </div>
+</div>
+<!--*************Prevention Against Corona Virus***************-->
+<div class="container-fluid sub_section pt-5 pb-5 "id="preventid">
+	<div class="section_header text-center mb-5 mt-4">
+		<h1>6 steps Prevention Against Corona Virus</h1>
+	</div>
+	<div class="container">
+	    <div class="row">
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 6.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Wash your hands often with soap and water for at least 20 sec</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 2.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Cover your mouth and nose with a tissue</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 3.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Refrain from close contact do not shake hands</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 4.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Stay away from crowded places</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 5.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Donot touch your eyes,nose,or mouth</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+		    <div class="col-lg-4 col-md-4 col-12 mt-5">
+		    	<div class="row">
+		    		<div class="col-lg-4 col-md-4 col-12">
+		    			<figure class="text-center">
+				        <img src="images/corona per 7.jpg" class="img-fluid" width="90" height="90">
+			            </figure>
+		    		</div>
+		    		<div class="col-lg-8 col-md-8 col-12">
+		    			<p>Use hand sanitizer</p>
+		    		</div>
+		    	</div>
+		    </div>
+
+
+		</div>
+	</div>
+</div>
+<!--*************Contact Us ASAP***************-->
+<div class="container-fluid pt-5 pb-5 "id="contactid">
+	<div class="section_header text-center mb-5 mt-4">
+		<h1> Contact Us ASAP</h1>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 offset-lg-2 col-12">
+				<form action="" method="POST">
+  <div class="form-group">
+    <label>Username</label>
+    <input type="text" class="form-control" name="username" placeholder="name" autocomplete="off" required>
+  </div>
+
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" name="email" placeholder="name@example.com" reuired autocomplete="off">
+  </div>
+
+  <div class="form-group">
+    <label>Mobile Number</label>
+    <input type="number" class="form-control" name="mobile" placeholder="Number" autocomplete="off" required>
+  </div>
+
+  <div class="form-group">
+  	<label>Select Symptoms</label><br>
+
+  	<div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+  		<input type="checkbox" class="custom-control-input" id="customcheckbox1" name="coronasym[]" value="cold">
+  		<label class="custom-control-label" for="customcheckbox1">Cold</label>
+  	</div>
+
+    	<div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+  		<input type="checkbox" class="custom-control-input" id="customcheckbox2" name="coronasym[]" value="Fever">
+  		<label class="custom-control-label" for="customcheckbox2">Fever</label>
+  	</div>
+  	  	<div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+  		<input type="checkbox" class="custom-control-input" id="customcheckbox3" name="coronasym[]" value="breath">
+  		<label class="custom-control-label" for="customcheckbox3">Difficulty in breath</label>
+  	</div>
+  	  	<div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+  		<input type="checkbox" class="custom-control-input" id="customcheckbox4" name="coronasym[]" value="Tired">
+  		<label class="custom-control-label" for="customcheckbox4">Feeling weak</label>
+  	</div>
+  </div>
+   
+  <div class="form-group">
+    <label>Example text</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="msg"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+
+			</div>
+		</div>
+	</div>
+
+        
+
+</div>
+<!--*************Footer***************-->
+<footer class="mt-5">
+	<div class="footer_style text-white text-center container-fluid">
+		<p>copyright by AkhonAbuzar</p>
+		
+	</div>
+	
+</footer>
+
+
+</body>
+</html>
+
+
+
+<?php
+
+include 'dbcon.php';
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+	$symptoms = $_POST['coronasym'];
+	$Message = $_POST['msg'];
+
+	$chk ="";
+	foreach ($symptoms as $chk1) {
+		$chk .=$chk1.",";
+	}
+
+	$insertquery="insert into coronacase(username,email,mobile,symptoms,Message) values('$username','$email','$mobile','$chk','$Message')";
+
+	$query= mysqli_query($con,$insertquery);
+
+	if ($query) {
+	?>
+	<script>
+		alert("inserted succesful");
+	</script>
+	<?php
+}
+else{
+	?>
+	<script>
+		alert("Not inserted");
+	</script>
+	<?php
+}
+
+}
+?>
